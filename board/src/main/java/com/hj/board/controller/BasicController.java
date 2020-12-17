c4bbf44d9562bb3f975fa573dce4a0fa98390d37
heyday05 <maybloom05@gmail.com>
2020-12-17 23:36:57 +0900

    day2

---
+++ board/src/main/java/com/hj/board/controller/BasicController.java
@@ -0,0 +1,14 @@
+package com.hj.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+    
+    @GetMapping("/home")
+    public String home(){
+
+        return "Hello world";
+    }
+}
